cb6d979e3d9ae1d3a29e281ba3ad15faa024c656
MPS Vihanga <>
2024-03-31 02:28:47 +0530

    setting up timer with useEffect

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
 import FinishedScreen from "./components/FinishedScreen";
+import Timer from "./components/Timer";
+import Footer from "./components/Footer";
 
 const initialState = {
     questions: [],
@@ -15,7 +17,8 @@ const initialState = {
     index: 0,
     answer: null,
     points: 0,
-    highscore: 0
+    highscore: 0,
+    secondsRemaining: 10,
 }
 function reducer(state, action) {
     switch (action.type) {
@@ -28,8 +31,7 @@ function reducer(state, action) {
         case 'newAnswer':
             const question = state.questions.at(state.index);
             return {
-                ...state,
-                answer: action.payload,
+                ...state, answer: action.payload,
                 points: action.payload === question.correctOption ? state.points + question.points : state.points
             };
         case 'nextQuestion' :
@@ -42,6 +44,9 @@ function reducer(state, action) {
             }
         case 'restart': return {...initialState, question : state.questions, status: 'ready'};
                         /*return {...state, points: 0, highscore: 0, index: 0, answer: null, status: 'ready'}*/
+        case 'tick':
+            return {...state, secondsRemaining: state.secondsRemaining -1,
+                status: state.secondsRemaining === 0 ? 'finished' : state.status};
 
         default :
             throw new Error("Action Unknown !");
@@ -50,7 +55,7 @@ function reducer(state, action) {
 
 export default function App() {
 
-    const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer, points, highscore, secondsRemaining}, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
     const macPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
@@ -75,10 +80,14 @@ export default function App() {
                         <Progress index={index} numQuestions={numQuestions} points={points}
                                     maxPossiblePoints={macPossiblePoints} answer={answer}/>
                         <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
-                        <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions}/>
+                        <Footer>
+                            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+                            <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions}/>
+                        </Footer>
                     </>
                 }
-                {status === 'finished' && <FinishedScreen points={points} maxPossiblePoints={macPossiblePoints} dispatch={dispatch()}/>}
+                {status === 'finished' && <FinishedScreen points={points}
+                                                            maxPossiblePoints={macPossiblePoints} dispatch={dispatch()}/>}
             </Main>
         </div>
     );

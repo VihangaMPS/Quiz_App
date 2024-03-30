020385979fd06b7dbc4873d4587e303d367a991e
MPS Vihanga <>
2024-03-30 23:30:31 +0530

    moving to the next button

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import Loader from "./components/Loader";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import question from "./components/Question";
+import NewButton from "./components/NewButton";
 
 function reducer(state, action) {
     switch (action.type) {
-        case 'dataReceived': return {...state, questions: action.payload, status: 'ready'};
-        case 'dataFailed': return {...state, status: 'error'};
-        case 'start': return {...state, status: 'active'};
+        case 'dataReceived':
+            return {...state, questions: action.payload, status: 'ready'};
+        case 'dataFailed':
+            return {...state, status: 'error'};
+        case 'start':
+            return {...state, status: 'active'};
         case 'newAnswer':
             const question = state.questions.at(state.index);
-            return {...state, answer: action.payload, points: action.payload === question.correctOption ? state.points +question.points : state.points};
+            return {
+                ...state,
+                answer: action.payload,
+                points: action.payload === question.correctOption ? state.points + question.points : state.points
+            };
+        case 'nextQuestion' :
+            return {...state, index: state.index + 1, answer: null};
 
-        default : throw new Error("Action Unknown !");
+        default :
+            throw new Error("Action Unknown !");
     }
 }
 
@@ -30,7 +41,7 @@ const initialState = {
 
 export default function App() {
 
-    const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
 
@@ -49,7 +60,12 @@ export default function App() {
                 {status === 'loading' && <Loader/>}
                 {status === 'error' && <Loader/>}
                 {status === 'ready' && <StartScreen numOfQuestions={numQuestions} dispatch={dispatch}/>}
-                {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer}/>}
+                {status === 'active' &&
+                    <>
+                        <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
+                        <NewButton dispatch={dispatch} answer={answer}/>
+                    </>
+                }
             </Main>
         </div>
     );

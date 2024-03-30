9025cf609255c65b2b2ca914feceb3008e8828fa
MPS Vihanga <>
2024-03-30 10:02:26 +0530

    Starting a new quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+import Loader from "./Loader";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived': return {...state, questions: action.payload, status: 'ready'};
+        case 'dataFailed': return {...state, status: 'error'};
+        case 'start': return {...state, status: 'active'};
+
+        default : throw new Error("Action Unknown !");
+    }
+}
+
+const initialState = {
+    questions: [],
+    // loading, error, ready, active, finished
+    status: 'loading'
+}
 
 export default function App() {
-    return (
-        <div>
 
+    const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length;
+
+    useEffect(() => {
+        fetch("http://localhost:8000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({type: 'dataReceived', payload: data}))
+            .catch((err) => dispatch({type: 'dataFailed'}));
+    }, []);
+
+    return (
+        <div className="app">
+            <Header/>
+
+            <Main>
+                {status === 'loading' && <Loader/>}
+                {status === 'error' && <Loader/>}
+                {status === 'ready' && <StartScreen numOfQuestions={numQuestions} dispatch={dispatch}/>}
+                {status === 'active' && <Question/>}
+            </Main>
         </div>
     );
 }
